767df805ae2ab764b652833cb3afadd5dfd6d73c
Cyril Demand <cyril.demand.etu@univ-lille.fr>
2021-04-18 00:47:54 +0200

    test discord

--- src/moulin/Jeu.java
+++ src/moulin/Jeu.java
@@ -3,6 +3,7 @@ package moulin;
 public class Jeu {
 
     public static void main(String[] args) {
+        //test discord
         Board board = new Board();
         Player player1 = new Player("Pierre",Color.BLANC);
         Player player2 = new Player("Morgane",Color.BLEU);
