904366f8dac1a132da29325dac2657d12cb1e224
Noe Delcroix <noe.delcroix.etu@univ-lille.fr>
2021-04-21 16:52:42 +0200

    classe Node modifiée

--- src/moulin/Node.java
+++ src/moulin/Node.java
@@ -2,6 +2,15 @@ package moulin;
 
 public class Node {
     private int x;
-
     private int y;
+
+    private int id;
+    private static int counter;
+
+    Node(int x,int y){
+        this.x=x;
+        this.y=y;
+        this.id=Node.counter;
+        counter++;
+    }
 }
