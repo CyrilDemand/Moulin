09802511383b279139c9f9d24cf9850ee273601a
Noe Delcroix <noe.delcroix.etu@univ-lille.fr>
2021-06-02 11:58:02 +0200

    encore meilleur main

--- src/moulin/Main.java
+++ src/moulin/Main.java
@@ -10,39 +10,63 @@ public class Main {
      * @throws Exception exception
      */
     public static void main(String[] args) throws Exception {
-
-        Player resultOfTheGame=null;
-
-        Board board = Board.generateBoard(4);
-        ArrayList<Player> players = new ArrayList<>();
-        players.add(new Player("Patrick",Color.ROUGE));
-        players.add(new RandomAI("Intelligence Artificielle",Color.BLEU));
-
-        for (int i=0;i<3;i++){
-            for (Player p:players) {
-                p.addPiece(new Piece(p.getColor(),i));
-            }
-        }
-
-        Jeu jeu = new Jeu(board,players);
-        System.out.println(jeu.getPlayers());
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Choisissez votre methode de placement des pions : ");
-        System.out.println("[1] Placement par les joueurs");
-        System.out.println("[2] Placement aléatoire");
+        System.out.println("Bienvenue sur le jeu du moulin !\n");
+        System.out.println("Que voulez-vous faire ?");
+
+        System.out.println("[1] Nouvelle partie");
+        System.out.println("[2] Charger une sauvergarde");
+        System.out.println("[3] Quitter");
+
         int choix;
         do {
             System.out.print("Votre choix : ");
             choix = scanner.nextInt();
-        }while (!(choix>=1 && choix<=2));
+        }while (!(choix>=1 && choix<=3));
+
+
+        Player resultOfTheGame=null;
+        Jeu jeu=null;
 
         if (choix==1){
-            resultOfTheGame=jeu.start();
-        }else {
-            jeu = Jeu.randomStart(jeu.getBoard(),jeu.getPlayers());
+            //-------NOUVELLE PARTIE--------
+
+
+            Board board = Board.generateBoard(4);
+            ArrayList<Player> players = new ArrayList<>();
+            players.add(new Player("Patrick",Color.ROUGE));
+            players.add(new RandomAI("Intelligence Artificielle",Color.BLEU));
+
+            for (int i=0;i<3;i++){
+                for (Player p:players) {
+                    p.addPiece(new Piece(p.getColor(),i));
+                }
+            }
+
+            jeu = new Jeu(board,players);
+            System.out.println(jeu.getPlayers());
+
+
+            System.out.println("Choisissez votre methode de placement des pions : ");
+            System.out.println("[1] Placement par les joueurs");
+            System.out.println("[2] Placement aléatoire");
+            do {
+                System.out.print("Votre choix : ");
+                choix = scanner.nextInt();
+            }while (!(choix>=1 && choix<=2));
+
+            if (choix==1){
+                resultOfTheGame=jeu.start();
+            }else {
+                jeu = Jeu.randomStart(jeu.getBoard(), jeu.getPlayers());
+            }
+        }else if (choix==2){
+            //----------CHARGER UNE PARTIE EXISTANTE------------------
+        }else{
+            return;
         }
 
+        //-------------BOUCLE DE JEU-------------------
         if (resultOfTheGame==null){
             resultOfTheGame=jeu.endGame();
         }
@@ -50,10 +74,9 @@ public class Main {
         if (resultOfTheGame==null){
 
         }else{
-            board.render(3,1);
+            jeu.getBoard().render(3,1);
             System.out.println(resultOfTheGame+" a gagné !");
         }
-
 
     }
 
