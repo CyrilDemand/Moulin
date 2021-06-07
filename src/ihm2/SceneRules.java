e4fd2be6a962c9e1ada12fee251c54bb19de83ad
Noe Delcroix <noe.delcroix.etu@univ-lille.fr>
2021-06-07 21:01:52 +0200

    debut ihm

--- src/ihm2/SceneRules.java
+++ src/ihm2/SceneRules.java
@@ -4,19 +4,18 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
-public class SceneTest {
+public class SceneRules {
 
     private static Scene scene;
 
     public static void create(){
         VBox root=new VBox();
-
-        root.getChildren().add(new Label("Test"));
-
-        SceneTest.scene=new Scene(root,1000,500);
+        root.getChildren().add(new Label("Rules"));
+        scene=new Scene(root,1000,500);
     }
 
     public static Scene getScene(){
-        return SceneTest.scene;
+        return scene;
     }
+
 }
