b706f75fddbcdfc314aa3daf0b5d19fc7d5478f0
Cyril Demand <cyril.demand.etu@univ-lille.fr>
2021-06-08 23:13:10 +0200

    debut pas fonctionnel

--- src/ihm2/SceneStartGame.java
+++ src/ihm2/SceneStartGame.java
@@ -1,17 +1,25 @@
 package ihm2;
 
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import moulin.Config;
+import moulin.Node;
+import moulin.Player;
+import moulin.RandomAI;
 
 public class SceneStartGame {
     private static Scene scene;
-
+    static CustomCanvas customCanvas;
+    private static VBox root;
+    private static int i;
+    private static boolean posed;
     public static void create(){
-        VBox root=new VBox();
-        CustomCanvas customCanvas = new CustomCanvas(500,500);
+        root=new VBox();
+        customCanvas = new CustomCanvas(500,500);
         root.getChildren().add(customCanvas);
-        customCanvas.render(SceneNewGame.getJeu());
 
         scene=new Scene(root,Main.getDefaultSceneWidth(),Main.getDefaultSceneHeight());
     }
@@ -21,6 +29,40 @@ public class SceneStartGame {
     }
 
     public static void switchTo(){
+        customCanvas.render(SceneNewGame.getJeu());
         Main.changeScene(scene);
+        for(i = 0; i<SceneNewGame.getJeu().getPlayers().get(0).getPieces().size();i++){
+            for (Player p: SceneNewGame.getJeu().getPlayers()) {
+                System.out.println(p.getClass().getSimpleName());
+                if (p.getClass().getSimpleName().equals("Player")){
+                    posed=false;
+                    root.setOnMousePressed(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent mouseEvent) {
+                            customCanvas.render(SceneNewGame.getJeu());
+                            Main.changeScene(scene);
+                            Node n = customCanvas.mouseOverNode(SceneNewGame.getJeu());
+                            if (n != null) {
+                                System.out.println("dans le null");
+                                p.getPieces().get(i).put(n);
+                                posed = true;
+                            }
+                        }
+                    });
+                }if (posed){
+                    ((RandomAI)p).start(SceneNewGame.getJeu(),p.getPieces().get(i));
+                }
+                try {
+                    Player winner = SceneNewGame.getJeu().isFinished();
+                    if(winner!=null){
+                        Alert a = new Alert(Alert.AlertType.INFORMATION,"AND THE WINNER IS : " + winner.getName());
+                        a.setTitle("Winner !");
+                        a.show();
+                    }
+                }catch (Exception ignored){
+
+                }
+            }
+        }
     }
 }
