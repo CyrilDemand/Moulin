71b7379dee47310e968d2065f1b7703b16462ee8
Cyril Demand <cyril.demand.etu@univ-lille.fr>
2021-06-08 08:37:15 +0200

    chargement auto

--- src/ihm2/SceneSidesMap.java
+++ src/ihm2/SceneSidesMap.java
@@ -2,10 +2,14 @@ package ihm2;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Spinner;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import moulin.Board;
 import moulin.Jeu;
+
+import java.awt.*;
 
 public class SceneSidesMap {
     private static Scene scene;
@@ -13,9 +17,11 @@ public class SceneSidesMap {
     public static void create(){
 
         VBox root=new VBox();
+        HBox buttonBar=new HBox();
+        Canvas canvas = new Canvas(100,100);
         Spinner<Integer> spinner = new Spinner<>(3,10,3,1);
-        Button loadButton = new Button("load");
-        loadButton.setOnAction(actionEvent -> {
+        spinner.valueProperty().addListener((obs, oldValue, newValue) ->
+                {
             Jeu jeu = new Jeu(null,null);
             int side = spinner.getValue();
             jeu.setBoard(Board.generateBoard(side));
@@ -26,11 +32,14 @@ public class SceneSidesMap {
         goBack.setOnAction(e->{
             Main.changeScene(SceneTypeOfMap.getScene());
         });
-        Button goBackToMainMenu =new Button("go back to main menu");
-        goBack.setOnAction(e->{
+        Button goBackToMainMenu=new Button("Go Back to main menu");
+        goBackToMainMenu.setOnAction(e->{
             Main.changeScene(SceneMainMenu.getScene());
         });
-        root.getChildren().addAll(spinner,loadButton,goBack,goBackToMainMenu);
+        Button next=new Button("Next");
+        next.setDisable(true);
+        buttonBar.getChildren().addAll(goBackToMainMenu,goBack,next);
+        root.getChildren().addAll(canvas,spinner,buttonBar);
         scene=new Scene(root,1000,500);
     }
 
